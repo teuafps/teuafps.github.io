642095c10b60bab92534f2f14e904ae5ebc4aae3
てうあ <yosh1as@outlook.jp>
2025-09-23 02:15:43 +0900

    v25

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,6 +1,13 @@
-import DefaultTheme from 'vitepress/theme'
-import './style.css'
+// docs/.vitepress/theme/index.ts
+
+import Theme from 'vitepress/theme';
+import RecruitCard from './components/RecruitCard.vue';
+import './custom.css'; // 必要に応じてグローバルスタイルを追加
 
 export default {
-  ...DefaultTheme
-}
+  ...Theme,
+  // 登録するコンポーネント
+  enhanceApp({ app }) {
+    app.component('RecruitCard', RecruitCard);
+  },
+};
